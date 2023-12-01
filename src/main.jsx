75884763f5fece7899a87a1399a815daff2723d0
Omar Martinez <omar1204@gmail.com>
2023-12-01 11:15:41 -0600

    all

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './css/style.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { Home, Error } from './pages'
+import { Home, Error, Contact } from './pages'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home/>,
         errorElement: <Error/>
+    },
+    {
+        path: 'contacts/:contactId',
+        element: <Contact/>
     },
 ])
 
